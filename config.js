4119b49b13aa17c43555d388658b63a9a609e1f0
Luka Jerkovic <luka.jerkovic@sistemi.hr>
2015-08-24 11:05:59 +0200

    test 13

--- config.js
+++ config.js
@@ -1,36 +1,36 @@
 var config = {
 
-	// Your site title (format: page_title - site_title)
-	site_title: 'Raneto Docs',
+  // Your site title (format: page_title - site_title)
+  site_title: 'SistemiKB',
 
-	// The base URL of your site (can use %base_url% in Markdown files)
-	base_url: '',
+  // The base URL of your site (can use %base_url% in Markdown files)
+  base_url: '',
 
-	// Used for the "Get in touch" page footer link
-	support_email: '',
+  // Used for the "Get in touch" page footer link
+  support_email: '',
 
-	// Footer copyright content
-	copyright: 'Copyright &copy; '+ new Date().getFullYear() +' - <a href="http://google.com">Powered by Raneto</a>',
+  // Footer copyright content
+  copyright: 'Copyright &copy; ' + new Date().getFullYear() + ' - <a href="http://google.com">Powered by Raneto</a>',
 
-	// Excerpt length (used in search)
-	excerpt_length: 400,
+  // Excerpt length (used in search)
+  excerpt_length: 400,
 
-	// The meta value by which to sort pages (value should be an integer)
-	// If this option is blank pages will be sorted alphabetically
-	page_sort_meta: 'sort',
+  // The meta value by which to sort pages (value should be an integer)
+  // If this option is blank pages will be sorted alphabetically
+  page_sort_meta: 'sort',
 
-	// Should categories be sorted numerically (true) or alphabetically (false)
-	// If true category folders need to contain a "sort" file with an integer value
-	category_sort: true,
+  // Should categories be sorted numerically (true) or alphabetically (false)
+  // If true category folders need to contain a "sort" file with an integer value
+  category_sort: true,
 
-	// The base URL of your images folder (can use %image_url% in Markdown files)
-	image_url: '/images',
+  // The base URL of your images folder (can use %image_url% in Markdown files)
+  image_url: '/images',
 
-	// Specify the path of your content folder where all your '.md' files are located
-	content_dir: './content/',
+  // Specify the path of your content folder where all your '.md' files are located
+  content_dir: './site/wwwroot/content/',
 
-	// Add your analytics tracking code (including script tags)
-	analytics: ""
+  // Add your analytics tracking code (including script tags)
+  analytics: ""
 
 };
 
